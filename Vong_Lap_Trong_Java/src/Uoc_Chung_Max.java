2a0b90f0594eed141f5113f3b1e4e11efc3f6496
dinhson98 <Dinhsonmta55@gmail.com>
2023-05-04 22:58:25 +0700

    c0323h1

---
+++ Vong_Lap_Trong_Java/src/Uoc_Chung_Max.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Uoc_Chung_Max {
+    public static void main(String[] args) {
+        int a;
+        int b;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Nhap so a : ");
+        a = input.nextInt();
+        System.out.println("Nhap so b : ");
+        b = input.nextInt();
+
+        a = Math.abs(a);
+        b = Math.abs(b);
+
+        if (a == 0 || b == 0) {
+            System.out.println("No greatest common factor");
+        }
+        while (a != b) {
+            if (a > b)
+                a = a - b;
+            else
+                b = b - a;
+        }
+        System.out.println("Greatest common factor: " + a);
+    }
+}
